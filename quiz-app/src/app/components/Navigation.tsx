a752637c223478fac322c0d94ac47ef2ddccb970
gergokoncz <gergokoncz06@gmail.com>
2023-11-28 23:47:22 +0100

    updated navigation

--- quiz-app/src/app/components/Navigation.tsx
+++ quiz-app/src/app/components/Navigation.tsx
@@ -1,41 +1,58 @@
 
-import React from "react";
+"use client";
+
+import React, { use } from "react";
 import Link from "next/link";
-import Button from "./button";
+import Image from "next/image";
 import Logo from "./logo";
+
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Navigation = () => {
     return (
       <>
-        <div className="w-full h-20 bg-dfds-main sticky top-0">
-          <div className="container mx-auto px-4 h-full">
-            <div className="flex justify-between items-center h-full">
-              <Logo />
-              <ul className="hidden md:flex gap-x-6 text-white">
-                <li className="flex items-center h-12 rounded-lg bg-white font-bold px-5 align-middle hover:bg-dfds-mid"> 
-                  <Link href="/admin" className="no-underline text-dfds-main">
-                    <p>Create Quiz</p>
-                  </Link>
-                </li>
-                <li className="flex items-center h-12 rounded-lg bg-white font-bold px-5 align-middle hover:bg-dfds-mid">
-                  <Link href="/quiz" className="no-underline text-dfds-main">
-                    <p>Take Quiz</p>
-                  </Link>
-                </li>
-                <li className="flex items-center h-12 rounded-lg bg-white font-bold px-5 align-middle hover:bg-dfds-mid">
-                  <Link href="/quiz" className="no-underline text-dfds-main">
-                    <p>Run Quiz</p>
-                  </Link>
-                </li>
-                <li className="flex items-center h-12 rounded-lg bg-white font-bold px-5 align-middle hover:bg-dfds-mid">
-                  <Link href="/contact" className="no-underline text-dfds-main">
-                    <p>Contact</p>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Navbar expand="md" variant="dark" className="w-full min-h-20 bg-dfds-main sticky top-0">
+          <Container fluid>
+              <Navbar.Brand href="/">
+                <Container className="flex flex-col text-center">
+                  <Image
+                    src="/dfdslogo.png"
+                    alt="Logo"
+                    width={80}
+                    height={80}
+                  />
+                </Container>
+              </Navbar.Brand>
+              <Navbar.Toggle
+                aria-controls="navbarScroll"
+                data-bs-target="#navbarScroll"
+              />
+              <Navbar.Collapse id="navbarScroll" className="ms-auto">
+                <Nav className="ml-auto">
+                  <Nav.Item className="my-2 mr-4"> 
+                    <Link href="/admin" className="flex items-end justify-center no-underline text-dfds-main h-12 rounded-lg bg-white font-bold px-4 align-middle hover:text-dfds-light shadow-inner">
+                      <p>Create Quiz</p>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item className="my-2 mr-4">
+                    <Link href="/quiz" className="flex items-end justify-center no-underline text-dfds-main h-12 rounded-lg bg-white font-bold px-4 align-middle hover:text-dfds-light shadow-xl">
+                      <p>Take Quiz</p>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item className="my-2 mr-4">
+                    <Link href="/quiz" className="flex items-end justify-center no-underline text-dfds-main h-12 rounded-lg bg-white font-bold px-4 align-middle hover:text-dfds-light shadow-xl">
+                      <p>Run Quiz</p>
+                    </Link>
+                  </Nav.Item>
+                  <Nav.Item className="my-2 mr-4">
+                    <Link href="/contact" className="flex items-end justify-center no-underline text-dfds-main h-12 rounded-lg bg-white font-bold px-4 align-middle hover:text-dfds-light shadow-xl">
+                      <p>Contact</p>
+                    </Link>
+                  </Nav.Item>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+        </Navbar>
       </>
     );
   };
